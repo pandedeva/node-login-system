5f07fed70e9c2248300ab21b6e1ac671fa497ab6
deva <devapande66@gmail.com>
2021-12-01 23:42:49 +0800

    initial commit

---
+++ passport-config.js
@@ -0,0 +1,28 @@
+const localStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
+
+function initialize(passport, getUserByEmail) {
+  const authenticateUser = async (email, password, done) => {
+    const user = getUserByEmail(email);
+    if (user == null) {
+      return done(null, false, { message: "Email Salah!" });
+    }
+
+    try {
+      // kalau berhasil
+      if (await bcrypt.compare(password, user.password)) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: "Password Salah!" });
+      }
+    } catch (error) {
+      return done(error);
+    }
+  };
+
+  passport.use(new localStrategy({ usernameField: "email" }, authenticateUser));
+  passport.serializeUser((user, done) => {});
+  passport.deserializeUser((id, done) => {});
+}
+
+module.exports = initialize;
