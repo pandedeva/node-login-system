c41c198ca1de1820e5048799731df718bb608049
deva <devapande66@gmail.com>
2021-12-04 15:35:10 +0800

    sudah jadi

--- app.js
+++ app.js
@@ -8,12 +8,15 @@ const bcrypt = require("bcrypt");
 const passport = require("passport");
 const flash = require("express-flash");
 const session = require("express-session");
+const methodOverride = require("method-override");
 
 const initializePassport = require("./passport-config");
 const { Passport } = require("passport");
-initializePassport(passport, (email) => {
-  return users.find((user) => user.email === email);
-});
+initializePassport(
+  passport,
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
+);
 
 const users = [];
 
@@ -29,17 +32,20 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(methodOverride("_method"));
 
-app.get("/", (req, res) => {
-  res.render("index.ejs", { name: "kyle" });
+// ke halaman login dulu, baru ke dashboard
+app.get("/", checkAuthenticated, (req, res) => {
+  res.render("index.ejs", { name: req.user.name });
 });
 
-app.get("/login", (req, res) => {
+app.get("/login", checkNotAuthenticated, (req, res) => {
   res.render("login.ejs");
 });
 
 app.post(
   "/login",
+  checkNotAuthenticated,
   passport.authenticate("local", {
     successRedirect: "/",
     failureRedirect: "/login",
@@ -47,18 +53,18 @@ app.post(
   })
 );
 
-app.get("/register", (req, res) => {
+app.get("/register", checkNotAuthenticated, (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", async (req, res) => {
+app.post("/register", checkNotAuthenticated, async (req, res) => {
   try {
     // membuat password acak
     const hashPassword = await bcrypt.hash(req.body.password, 10);
     users.push({
       // kalau pakai database, tidak usah isi ini
       id: Date.now().toString(),
-      name: req.body.nama,
+      name: req.body.name,
       email: req.body.email,
       password: hashPassword,
     });
@@ -67,7 +73,34 @@ app.post("/register", async (req, res) => {
   } catch (error) {
     res.redirect("/register");
   }
-  console.log(users);
+  console.log(users); // menampilkan akun yang baru di register
 });
+
+app.delete("/logout", (req, res) => {
+  req.logOut();
+  res.redirect("/login");
+});
+
+// kalau user sudah daftar
+function checkAuthenticated(req, res, next) {
+  // true
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  // false
+  res.redirect("/login");
+}
+
+// kalau user sudah login di refresh tidak akan kembali ke login
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    // kembali ke homepage
+    return res.redirect("/");
+  }
+
+  // kembali ke login
+  next();
+}
 
 app.listen(3000);
